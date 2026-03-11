6e4d2717447dc657921e7228923c24e4484172df
Ahsan Alam <ahsanulalam.500@gmail.com>
2026-03-11 14:30:29 +0600

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,72 @@
+export const NAME = "AHSAN UL ALAM";
+
+export const DESIGNATION = `Software Architect | MERN Stack Developer | Laravel Developer | Open Source Contributor`;
+
+export const DETAILS = {
+  Residence: "Bangladesh",
+  City: "Dhaka",
+  Age: "25",
+};
+
+export const SKILLS = [
+  {
+    title: "MERN Stack Developer",
+    level: "75%",
+  },
+  {
+    title: "Laravel Developer",
+    level: "80%",
+  },
+  {
+    title: "React Developer",
+    level: "91%",
+  },
+  {
+    title: "Backend Developer",
+    level: "54%",
+  },
+  {
+    title: "Open Source Contributor",
+    level: "85%",
+  },
+  {
+    title: "Database Management",
+    level: "75%",
+  },
+];
+
+export const TECH_STACK = [
+  "JavaScript",
+  "PHP",
+  "Laravel",
+  "ReactJS",
+  "NextJS",
+  "TypeScript",
+  "NodeJS",
+  "Redux",
+  "Zustand",
+  "CSS",
+  "SCSS",
+  "TailwindCSS",
+  "Ant Design",
+  "Material UI",
+  "Next UI",
+  "REST API",
+  "MongoDB",
+  "MySQL",
+  "Git",
+  "GitHub",
+  "Jira",
+  "Figma",
+  "Adobe XD",
+];
+
+export const CONTACTS = {
+  EMAIL: "ahsanulalam.500@gmail.com",
+  PHONE: "01773766658",
+};
+export const SOCIAL_LINKS = {
+  GITHUB: "https://github.com/ahsan-ul-alam",
+  LINKEDIN: "https://www.linkedin.com/in/ahsan-ul-alam",
+  FACEBOOK: "https://www.facebook.com/ahsan.alam.official",
+};
